d3bb854fc5cb82a4af395bf50e7450e3395a4dee
xuyuji <xuyuji1987@gmail.com>
2019-03-17 20:41:39 +0800

    add final

--- Creational/singleton/src/main/java/org/xuyuji/pattern/singleton/EagerInitializedSingleton.java
+++ Creational/singleton/src/main/java/org/xuyuji/pattern/singleton/EagerInitializedSingleton.java
@@ -2,7 +2,7 @@ package org.xuyuji.pattern.singleton;
 
 public class EagerInitializedSingleton {
 
-	private static EagerInitializedSingleton instance = new EagerInitializedSingleton();
+	private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
 
 	private EagerInitializedSingleton() {
 	}
